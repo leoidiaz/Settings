285e409d8422ec2a9baefd05a80669ad75ace26e
leoidiaz <zaid.leonardo@gmail.com>
2020-04-18 15:22:39 -0700

    Add SettingCellDelegate Protocol

--- Settings/Controllers/ViewControllers/Custom Views/SettingTableViewCell.swift
+++ Settings/Controllers/ViewControllers/Custom Views/SettingTableViewCell.swift
@@ -8,13 +8,20 @@
 
 import UIKit
 
+protocol SettingCellDelegate: class {
+    // Step 1 Declare Protocol
+    func settingSwitchToggled(for cell: SettingTableViewCell)
+}
+
 class SettingTableViewCell: UITableViewCell {
-    
     //MARK: - Outlets
     @IBOutlet weak var settingNameLabel: UILabel!
     @IBOutlet weak var settingImageView: UIImageView!
     @IBOutlet weak var settingSwitch: UISwitch!
     
+    // Step 2 Create the delegate
+    
+    weak var delegate: SettingCellDelegate?
     
     func updateViews(with setting: Setting) {
         settingNameLabel.text = setting.name
@@ -26,6 +33,7 @@ class SettingTableViewCell: UITableViewCell {
     
     //MARK: - Action
     @IBAction func settingSwitchToggled(_ sender: Any) {
+        delegate?.settingSwitchToggled(for: self)
     }
     
 }
